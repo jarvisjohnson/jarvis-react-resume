623b158d90bdeca2bcf88b7daf847cb88743f87c
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-05-04 16:52:05 -0400

    initial layout

--- src/shared/Profile/index.jsx
+++ src/shared/Profile/index.jsx
@@ -4,10 +4,36 @@ import * as styles from './styles.css'
 import resume from 'shared/content/resume'
 import myImage from '../../App/images/jarvis-johnson-profile.jpg'
 
+const makeList = (m, i) => <li key={i}>{m}</li>
+
+const mapTo = SomeComponent => (props, i) => 
+  <SomeComponent key={i} {...props} />
+
+const Social = ({network, username, url}) =>
+<div>
+  <h3>{category}</h3>
+  <ul className={styles.gridList}>
+    {keywords.reduce((list, next, i, original) => {
+      var halfway = Math.round(original.length / 2)
+
+      if (i < halfway)
+        list.push([next])
+      else
+        list[i - halfway].push(next)
+
+      return list
+    }, []).map((arr, i) =>
+      <div key={i} className={styles.gridListColumn}>
+        {arr.map(makeList)}
+      </div>
+    )}
+  </ul>
+</div>  
+
 const getLocationString = ({city, region, postalCode}) =>
 	`${city}, ${region} ${postalCode}`
 
-const Profile = ({name, label, email, phone, website, summary, location, picture}) =>
+const Profile = ({name, label, email, phone, website, summary, location, picture, socials}) =>
 	<div className={styles.profile}>
 		<img src={myImage} className={styles.profile_image}/>
 		<h1>{name}</h1>
@@ -23,6 +49,7 @@ const Profile = ({name, label, email, phone, website, summary, location, picture
 				{getLocationString(location)}
 			</li>
 		</ul>
+		{socials.map(mapTo(Social))}
 	</div>
 
 export default () => <Profile {...resume.basics} />
